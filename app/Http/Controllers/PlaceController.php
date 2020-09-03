04f4cd9bb850ab7085958c56c01b0c83045966af
Cr0cky <39223026+Cr0cky@users.noreply.github.com>
2020-09-03 15:00:19 +0300

    move randomplace.blade.php

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PlaceController extends Controller
 {
     public function index(){
-        return view('randomplace');
+        return view('places.randomplace');
     }
 }
